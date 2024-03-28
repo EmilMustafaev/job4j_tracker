6497771e8993f246f1cbb3f37c663b8575283bae
EmilMustafaev <emilmustafaev891@gmail.com>
2024-03-28 11:15:15 +0300

    1.7. Конструктор [#173889]

--- src/main/java/ru/job4j/oop/Error.java
+++ src/main/java/ru/job4j/oop/Error.java
@@ -15,15 +15,15 @@ public class Error {
     }
 
     public void showInfo() {
-        System.out.println(active);
-        System.out.println((status));
-        System.out.println(message);
+        System.out.println("Active: " + active);
+        System.out.println(("Status: " + status));
+        System.out.println("Message: " + message);
     }
 
     public static void main(String[] args) {
         Error errorOne = new Error();
-        Error errorTwo = new Error(true, 5, "Message in errorTwo");
-        Error errorThree = new Error(false, 3, "Message in errorThree");
+        Error errorTwo = new Error(true, 5, "errorTwo");
+        Error errorThree = new Error(false, 3, "errorThree");
         errorOne.showInfo();
         errorTwo.showInfo();
         errorThree.showInfo();
