abda7e426ef44b8c92b99db9d9e3402932756e00
EmilMustafaev <emilmustafaev891@gmail.com>
2024-04-04 11:57:00 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,35 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book bookCleanCode = new Book("Clean Code");
+        Book book1984 = new Book("1984");
+        Book bookTheHobbit = new Book("The Hobbit");
+        Book bookDracula = new Book("Dracula");
+        Book[] books = new Book[4];
+        books[0] = bookCleanCode;
+        books[1] = book1984;
+        books[2] = bookTheHobbit;
+        books[3] = bookDracula;
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println("Book name: " + book.getName());
+        }
+
+        Book item = books[0];
+        books[0] = books[3];
+        books[3] = item;
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName());
+        }
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.getName().equals("Clean Code")) {
+                System.out.println(book.getName());
+            }
+        }
+    }
+}
