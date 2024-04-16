5cb0ac56f1c0f0b7ebfbf2e93910106ddee00088
EmilMustafaev <emilmustafaev891@gmail.com>
2024-04-16 14:08:21 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key.equals(value[index])) {
+                result = index;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("The element not be found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
+        try {
+            FindEl.indexOf(names, "Emil");
+        } catch (ElementNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
