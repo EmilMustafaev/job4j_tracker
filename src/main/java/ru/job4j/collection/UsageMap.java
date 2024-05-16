cb09cdbec641a4c7ae94726bf5956794e94298dc
EmilMustafaev <emilmustafaev891@gmail.com>
2024-05-16 11:19:36 +0300

    0. Map, HashMap [#212652 #496240]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("mustafaev.emil@mail.ru", "Mustafaev Emil' Rudemovich");
+        map.put("ivanov.ivan@mail.ru", "Ivanov Ivan Ivanovich");
+        map.put("petrov.oleg@mail.ru", "Petrov Oleg Petrovich");
+        map.put("ivanov.ivan@mail.ru", "Ivanov Ivan Semenovich");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(value);
