5dadc0cb1c7ee6ec1bd2a034e5ee8cb66235f9e4
EmilMustafaev <emilmustafaev891@gmail.com>
2024-06-03 16:00:05 +0300

    7. Уникальность элементов и сортировка. [#110225]

--- src/test/java/ru/job4j/stream/Profiles.java
+++ src/test/java/ru/job4j/stream/Profiles.java
@@ -1,6 +1,7 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,19 @@ public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(profile -> profile.getAddress())
+                .collect(Collectors.toList());
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles.stream()
+                .map(profile -> profile.getAddress())
+                .sorted(new Comparator<Address>() {
+                    @Override
+                    public int compare(Address o1, Address o2) {
+                        return o1.getCity().compareTo(o2.getCity());
+                    }
+                })
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
