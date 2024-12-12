3f3e2004d151f9c328fb24f51b8253e2f874a8ac
EmilMustafaev <emilmustafaev891@gmail.com>
2024-12-12 13:34:18 +0300

    2. @Setter, @Getter и другие [#504889]

---
+++ src/main/java/ru/job4j/lombock/Category.java
@@ -0,0 +1,18 @@
+package ru.job4j.lombock;
+
+
+import lombok.*;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+public class Category {
+
+    @Getter
+    @EqualsAndHashCode.Include
+    @NonNull
+    private int id;
+
+    @Getter
+    @Setter
+    private String name;
+}
